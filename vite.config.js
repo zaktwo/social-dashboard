d827cf70a00f2e19ca350db62baaa1b0f0376924
Zak Maaz <zaktwo@users.noreply.github.com>
2023-04-30 10:11:41 +0100

    123

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: process.env.NODE_ENV === 'prodution' ? '/vite-js' : '/',
-    publicDir: 'public',
+    base: "/"
 
 })
